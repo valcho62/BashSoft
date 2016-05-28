e1c85e5b154034e90e7c6dc2f484e31b3b1a097d
Valcho <Valcho@VALCHOPC>
2016-05-28 15:13:09 +0300

    Add project files.

---
+++ BashSoft/BashSoft.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class BashSoft
+{
+    public static void TraverseDir ( string directory)
+    {
+        Console.WriteLine($"--{directory}--");
+        string[] allFiles = Directory.GetFiles(directory);
+        foreach (var file in allFiles)
+        {
+            Console.WriteLine(file);
+        }
+
+        string[] allDirs = Directory.GetDirectories(directory);
+        foreach (var dir in allDirs)
+        {
+            TraverseDir(dir);
+        }
+    }
+    static void Main()
+    {
+        TraverseDir("c:/");
+    }
+}
+
